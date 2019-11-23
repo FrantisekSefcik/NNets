fd8f161d91546f49c446e15e33ab1c576fd5bf45
Pomichal <vajk.pomichal@gmail.com>
2019-11-24 00:31:52 +0100

    ADD: basic image batch generator

--- text_to_image/data/analyze.py
+++ text_to_image/data/analyze.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
+import cv2
 from PIL import Image
 import requests
 import io
-from IPython.core.display import HTML
-from IPython.display import clear_output
+import random
 
 label_names = pd.read_csv('./metadata/label_names.csv', header=None, names=['Code', 'Name'])
 
@@ -16,7 +17,6 @@ def load_data(ids_location, labels_location, relationship_location):
 
 
 def basic_analysis(ids, labels):
-
     print("number of images: " + str(ids.shape[0]))
     top_overall = top_labels_analysis(labels)
     print()
@@ -59,7 +59,6 @@ def find_image_ids_with_labels(df_images, df_labels, labels, file_name='image_id
                 f.write(item + ',\n')
             f.close()
             id_list = []
-            clear_output(wait=True)
             print(index, "/", len(df_images))
     return return_list
 
@@ -74,9 +73,10 @@ def decode_class_names(class_codes):
     return [label_names.loc[label_names['Code'] == class_code].iat[0, 1] for class_code in class_codes]
 
 
-def show_image(image_url, url_col='OriginalURL', width=600):
-    picture_coded = requests.get(image_url).content
-    return Image.open(io.BytesIO(picture_coded))
+def show_image(image_url):
+    picture_request = requests.get(image_url)
+    if picture_request.status_code == 200:
+        return Image.open(io.BytesIO(picture_request.content))
 
 
 def get_image_labels(image_id, labels_df):
@@ -93,18 +93,55 @@ def get_image_relationships(df_rel, image_id):
 def load_image(url):
     picture_request = requests.get(url)
     if picture_request.status_code == 200:
-        return np.array(Image.open(io.BytesIO(picture_request.content)))
+        image = np.array(bytearray(picture_request.content))
+        image = cv2.imdecode(image, -1)
+        return image
+    return None
 
 
-def image_array_generator(urls, batch_size=0):
+def normalize_image(cv2_image, min=-1, max=1, norm_type=cv2.NORM_MINMAX):
+    return cv2.normalize(cv2_image, None, alpha=min, beta=max, norm_type=norm_type, dtype=cv2.CV_32F)
+
+
+def resize_image(cv2_image, new_size=(300, 300), interpolation_mode=cv2.INTER_LINEAR):
+    return cv2.resize(cv2_image, new_size, interpolation_mode)
+
+
+def image_array_generator(urls, batch_size=0,
+                          resize=True, size=(300, 300), interpolation=cv2.INTER_LINEAR,
+                          normalize=True, min=-1, max=1, norm_type=cv2.NORM_MINMAX):
+    """
+    Generator prepares batches of images for NN
+
+    :param urls: list of urls of images
+    :param batch_size: int
+    :param resize: bool - defines whether image scaling should be used
+    :param size: tuple (int, int) - new size of the images, if resize is True
+    :param interpolation: interpolation method from cv2
+    :param normalize: bool - defines whether pixel values should be normalized
+    :param min: float - the min normalized value if normalize is true
+    :param max: float - the max normalized value if normalize is true
+    :param norm_type: normalization type from cv2
+
+    yields a batch of preprocessed images
+    """
     i = 0
     batch = []
     if batch_size == 0:
         batch_size = len(urls)
-    for URL in imageUrlGenerator():
-        if i > batch_size:
+    random.shuffle(urls)
+    for URL in urls:
+        print(URL)
+        if len(batch) >= batch_size:
             yield batch
             batch = []
             i = 0
-        batch.append(loadImage(URL))
+        image = load_image(URL)
+        if image is not None:
+            if resize:
+                image = resize_image(image, size, interpolation)
+            if normalize:
+                image = normalize_image(image, min, max, norm_type)
+            batch.append(image)
         i += 1
+    yield batch
