3eac95e2fe582166efb574fa446c4e1a2df32486
FrantisekSefcik <frantisek.sefcik@gmail.com>
2019-11-24 20:17:21 +0100

    CHANGE: tensorboard to training loop

--- text_to_image/src/models/train_helpers.py
+++ text_to_image/src/models/train_helpers.py
@@ -1,7 +1,7 @@
 from numpy.random import randint, randn
 import numpy as np
 import matplotlib.pyplot as plt
-
+import tensorflow as tf
 
 class CGanTrainer:
     def __init__(self, discriminator, generator, combined, batch_size, n_classes, noise_dim):
@@ -13,7 +13,7 @@ class CGanTrainer:
         self.n_classes = n_classes
         self.noise_dim = noise_dim
 
-    def train_step(self, real_images, real_labels):
+    def train_step(self, real_images, real_labels, batch_no):
 
         real_loss, _ = self.discriminator.train_on_batch([real_images, real_labels], np.ones((self.HALF_BATCH, 1)))
 
@@ -26,7 +26,8 @@ class CGanTrainer:
             [input_noise_gan, random_y_gan],
             np.ones((self.BATCH_SIZE, 1))
         )
-        print(real_loss, fake_loss, gan_loss)
+        print(batch_no, real_loss, fake_loss, gan_loss)
+        return real_loss, fake_loss, gan_loss
 
     def generate_fake_input(self, n_samples):
         fake_labels = randint(0, self.n_classes, n_samples)
@@ -35,16 +36,21 @@ class CGanTrainer:
         return fake_noise, fake_labels
 
     def save_and_show_plot(self, examples, n, epoch):
+        fig, ax = plt.subplots()
         for i in range(self.n_classes * n):
             plt.subplot(self.n_classes, n, 1 + i)
             plt.axis('off')
             plt.imshow(examples[i, :, :, 0], cmap='gray_r')
         plt.savefig('images/image_at_epoch_{:04d}.png'.format(epoch))
-        plt.show()
+        # plt.show()
+        return np.array(fig.canvas.renderer._renderer)
+
 
     def create_fig(self, epoch):
         noise, _ = self.generate_fake_input(self.n_classes * 10)
         labels = np.asarray([x for _ in range(self.n_classes) for x in range(10)])
         images = self.generator.predict([noise, labels])
         images = (images + 1) / 2.0
-        self.save_and_show_plot(images, 10, epoch)
+        return self.save_and_show_plot(images, 10, epoch)
+
+
--- text_to_image/v2/cDCGAN_train.py
+++ text_to_image/v2/cDCGAN_train.py
@@ -1,6 +1,10 @@
+
+from datetime import datetime
+
 from numpy import expand_dims
 from numpy.random import randint
 from tensorflow.keras.datasets.fashion_mnist import load_data
+import tensorflow as tf
 import sys
 
 sys.path.append("..")
@@ -31,7 +35,11 @@ def generate_real_samples(dataset, n_samples):
 
 
 # train the generator and discriminator
-def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batch=128):
+def train(g_model, d_model, gan_model, dataset, latent_dim, callbacks, n_epochs=100, n_batch=128):
+
+    logdir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_writer = tf.summary.create_file_writer(logdir + "/metrics")
+
     trainer = CGanTrainer(d_model, g_model, gan_model, batch_size=n_batch, n_classes=10, noise_dim=latent_dim)
     bat_per_epo = int(dataset[0].shape[0] / n_batch)
     half_batch = int(n_batch / 2)
@@ -39,9 +47,14 @@ def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batc
     for i in range(n_epochs):
         for j in range(bat_per_epo):
             [x_real, labels_real] = generate_real_samples(dataset, half_batch)
-            trainer.train_step(x_real, labels_real)
+            real_loss, fake_loss, gan_loss = trainer.train_step(x_real, labels_real, i)
+
+        with file_writer.as_default():
+            tf.summary.scalar('loss', data=real_loss, step=i)
+            tf.summary.scalar('fake loss', data=fake_loss, step=i)
+            tf.summary.scalar('gan loss', data=gan_loss, step=i)
+
         print('EPOCH:', i)
-        trainer.create_fig(i)
     g_model.save('cgan_generator.h5')
 
 
@@ -52,5 +65,9 @@ g_model = gan.generator
 gan_model = gan.combined
 dataset = load_real_samples()
 
+log_path = './log'
+callback = tf.keras.callbacks.TensorBoard(log_path)
+callback.set_model(gan_model)
+
 # train model
-train(g_model, d_model, gan_model, dataset, latent_dim)
+train(g_model, d_model, gan_model, dataset, latent_dim, callback, 3)
